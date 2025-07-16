1487cc564f796d3753e6fdb46249492c1aa159a1
YashikaTaneja <yashika979.be22@chitkara.edu.in>
2025-07-16 12:36:59 +0530

    Manually merged dev into main

---
+++ While.java
@@ -0,0 +1,19 @@
+public class While {
+    /* print table of 2 using while loop*/
+    
+    public static void main(String[] args){
+        
+        int a=2;
+        
+        while(a<=10){
+            int i=1; 
+            System.out.println(a + "*" +i+"="+a*i);
+            i++;
+
+
+            
+            
+        }
+        
+    }
+}
