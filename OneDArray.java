6e03105dd7d92c0ad4cbccf14343a85cfa698342
YashikaTaneja <yashika979.be22@chitkara.edu.in>
2025-07-17 12:41:14 +0530

    Manually merged dev into main

---
+++ OneDArray.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class OneDArray {
+    public static void main(String[]args){
+
+        int arr[]= {9,5,3,2,1};
+        System.out.println("the one d array using for loop is :");
+        for(int i=0;i<=4;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
